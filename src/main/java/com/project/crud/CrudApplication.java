848037ebf62ff086116637c0aff4e748b24e37f1
vins <sumbaluwudevin@gmail.com>
2024-11-15 02:13:33 +0700

    merged (rebase) the master/local to main/remote (existing)

--- src/main/java/com/project/crud/CrudApplication.java
+++ src/main/java/com/project/crud/CrudApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CrudApplication {
-
+	//push commit to main branch remote
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
 	}
