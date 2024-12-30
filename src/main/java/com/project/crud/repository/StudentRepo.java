a0c06db16c1dabba2a749dcfb9a5b9d2ebad6cdd
Devins11 <devinfcb42@gmail.com>
2024-12-31 06:58:56 +0700

    added two repo interfaces

---
+++ src/main/java/com/project/crud/repository/StudentRepo.java
@@ -0,0 +1,10 @@
+package com.project.crud.repository;
+
+import com.project.crud.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepo extends JpaRepository<Student, Integer> {
+
+}
